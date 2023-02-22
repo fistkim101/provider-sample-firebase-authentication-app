8506eeaab99a6f5c4894bfdbe63642d07c8f865f
fistkim101 <fistkim101@gmail.com>
2023-02-22 09:32:35 +0900

    signin screen

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,1 +1,2 @@
 export 'auth/auth.dart';
+export 'signin/signin.dart';
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_sample_firebase_authentication_app/screens/screens.dart';
+import 'package:validators/validators.dart';
+
+import '../enums/enums.dart';
+import '../models/models.dart';
+import '../providers/providers.dart';
+import '../utils/error_dialog.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -9,11 +17,142 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  String? _email, _password;
+
+  void _submit() async {
+    setState(() {
+      _autovalidateMode = AutovalidateMode.always;
+    });
+
+    final form = _formKey.currentState;
+
+    if (form == null || !form.validate()) return;
+
+    form.save();
+
+    print('email: $_email, password: $_password');
+
+    try {
+      await context
+          .read<SignInProvider>()
+          .signIn(email: _email!, password: _password!);
+    } on CustomError catch (e) {
+      errorDialog(context, e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('signIn'),
+    final signInState = context.watch<SignInProvider>().state;
+
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Form(
+                key: _formKey,
+                autovalidateMode: _autovalidateMode,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Image.asset(
+                      'assets/images/flutter_logo.png',
+                      width: 250,
+                      height: 250,
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Email required';
+                        }
+                        if (!isEmail(value.trim())) {
+                          return 'Enter a valid email';
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _email = value;
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Password required';
+                        }
+                        if (value.trim().length < 6) {
+                          return 'Password must be at least 6 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _password = value;
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: signInState.signInStatusType ==
+                              SignInStatusType.submitting
+                          ? null
+                          : _submit,
+                      child: Text(signInState.signInStatusType ==
+                              SignInStatusType.submitting
+                          ? 'Loading...'
+                          : 'Sign In'),
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextButton(
+                      onPressed: signInState.signInStatusType ==
+                              SignInStatusType.submitting
+                          ? null
+                          : () {
+                              Navigator.pushNamed(
+                                  context, SignUpScreen.routeName);
+                            },
+                      child: Text('Not a member? Sign up!'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(
+                          fontSize: 20.0,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
